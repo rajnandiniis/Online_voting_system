d832586e163efeb8eace630d4b42cf10dcc07f07
rajnandiniis <rajnandini1783@gmail.com>
2024-07-06 23:49:59 +0530

    first commit

---
+++ routes/dashboard.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Online_voting_system </title>
+</head>
+
+<body>
+  <button>Back</button>
+  <button>Log Out</button>
+  <h1>Online Voting System</h1>
+  <hr>
+  <div id="Profile"></div>
+  <div id="Group"></div>
+
+</body>
+
+</html>
